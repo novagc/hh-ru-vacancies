0711cb5fc383618490720d41d5da778e519d6414
novagc <novagc@yandex.ru>
2022-05-22 22:57:40 +0700

    add settings loading

--- HH/HH.Downloader/Program.cs
+++ HH/HH.Downloader/Program.cs
@@ -1,13 +1,35 @@
+using HH.DB.Models;
+using HH.Settings;
+
 namespace HH.Downloader
 {
     internal static class Program
     {
+        public static BasicSettings? Settings { get; private set; }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            if (File.Exists(BasicSettings.DefaultFileName))
+            {
+                MessageBox.Show(
+                    $"Файл {BasicSettings.DefaultFileName} не найден.{Environment.NewLine}Создайте его и запустите приложение заново!");
+                return;
+            }
+
+            Settings = SettingsLoader<BasicSettings>.Load();
+
+            if (Settings == null)
+            {
+                MessageBox.Show($"Файл {BasicSettings.DefaultFileName} имеет неизвестный формат");
+                return;
+            }
+
+            HhContext.Init(Settings.ConnectionString);
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
