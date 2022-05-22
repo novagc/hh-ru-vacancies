934ae8087605e131a291b3eaeee08612682fd3ee
novagc <novagc@yandex.ru>
2022-05-22 20:38:47 +0700

    add tools project

---
+++ HH/HH.Tools/Utils.cs
@@ -0,0 +1,11 @@
+﻿namespace HH.Tools
+{
+    public static class Utils
+    {
+        public static bool InRange(this int num, int from, int to) => num >= from && num <= to;
+        public static bool InRangeOrNull(this int? num, int from, int to) => num == null || num.Value.InRange(from, to);
+
+        public static bool InRangeOrNull(this int? num, int? from, int? to)
+            => num == null || num.Value.InRange(from ?? int.MinValue, to ?? int.MaxValue);
+    }
+}
