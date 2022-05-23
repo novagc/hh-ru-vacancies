4e2699952a9371badbab40ec854d025d232876cf
novagc <novagc@yandex.ru>
2022-05-23 15:06:12 +0700

    add specialization url

---
+++ HH/HH.Net/Requests/SpecializationRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HH.Net.Models;
+using HH.Net.Models.SpecializationModel;
+using Newtonsoft.Json;
+
+namespace HH.Net.Requests
+{
+    public class SpecializationRequest
+    {
+        private readonly List<SpecializationItem> _specializations;
+
+        public SpecializationRequest()
+        {
+            _specializations = new List<SpecializationItem>();
+        }
+
+        public async Task<List<SpecializationItem>> Send()
+        {
+            _specializations.Clear();
+
+            var response = await Requests.SendGet("http://api.hh.ru/specializations");
+            var specializations = JsonConvert.DeserializeObject<List<SpecializationResponse>>(response);
+
+            foreach (var specialization in specializations!)
+            {
+                _specializations.Add(new SpecializationItem
+                {
+                    Id = specialization.Id,
+                    Name = specialization.Name
+                });
+
+                _specializations.AddRange(specialization.Specializations);
+            }
+
+            return _specializations;
+        }
+    }
+}
