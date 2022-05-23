be652f6ea2dceea4533caa8190064ffc653f7363
novagc <novagc@yandex.ru>
2022-05-23 15:06:53 +0700

    change sending get request

--- HH/HH.Net/Requests/Requests.cs
+++ HH/HH.Net/Requests/Requests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace HH.Net.Requests
 {
@@ -16,7 +18,23 @@ namespace HH.Net.Requests
         public static async Task<string> SendGet(string url)
         {
             using var client = new HttpClient();
-            return await client.GetStringAsync(url);
+            
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("Connection", "keep-alive");
+            client.DefaultRequestHeaders.Add("User-agent", "PostmanRuntime/7.29.0");
+
+            var response = await client.GetAsync(url);
+            var text = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Debug.WriteLine(text);
+                throw new Exception("400");
+            }
+            else
+            {
+                return text;
+            }
         }
     }
 }
